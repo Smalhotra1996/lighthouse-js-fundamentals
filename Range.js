92266e74349b3ccb97ac4a4c0daa19b1c1ef725f
Sonam <sonam.malhotra2496@gmail.com>
2021-07-06 03:18:04 +0000

    taking a break

--- Range.js
+++ Range.js
@@ -1,6 +1,26 @@
 const range = function(start,end,step){
-    for (range = 0; range<100; range++)
-    range (0,100,1);
-    return range;
+    var num = [];
+    
+    if (start === undefined || end === undefined 
+        || step === undefined){
+            return num;
+    }
+
+    if (start > end){
+        return num;
+    }
+
+    if (step <= 0){
+        return num;
+    }
+
+    for (let counter = start; counter <= end; counter+=step ){
+        num.push(counter);
+    }
+
+    return num;
 }
+
 console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
--- ice-cream.js
+++ ice-cream.js
@@ -1,4 +1,4 @@
-const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"]
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"]
 console.log(iceCreamFlavours);
 iceCreamFlavours.push("root beer");
 console.log(iceCreamFlavours);
